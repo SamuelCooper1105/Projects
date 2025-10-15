4a6c8411af2ae8c7e2a8bfd37614c02ad14bf9d6
SamuelCooper1105 <coopercumens@gmail.com>
2025-10-15 09:50:09 -0400

    Hold On Im a little Rusty

---
+++ Rusty/rust_book/target/debug/deps/rust_book-1a7429e3e3013afd.d
@@ -0,0 +1,5 @@
+/home/cooper/Projects/Rusty/rust_book/target/debug/deps/rust_book-1a7429e3e3013afd.d: src/main.rs
+
+/home/cooper/Projects/Rusty/rust_book/target/debug/deps/librust_book-1a7429e3e3013afd.rmeta: src/main.rs
+
+src/main.rs:
---
+++ Rusty/rust_book/target/debug/deps/rust_book-d0198e4db2734a16.d
@@ -0,0 +1,5 @@
+/home/cooper/Projects/Rusty/rust_book/target/debug/deps/rust_book-d0198e4db2734a16.d: src/main.rs
+
+/home/cooper/Projects/Rusty/rust_book/target/debug/deps/librust_book-d0198e4db2734a16.rmeta: src/main.rs
+
+src/main.rs:
