395658955eacfcdddd94156649ca624725bbc15d
SamuelCooper1105 <coopercumens@gmail.com>
2025-10-27 15:26:35 -0400

    cmon now

--- Algorithms/SortingAlgo/inserition.cpp
+++ Algorithms/SortingAlgo/inserition.cpp
@@ -1,46 +1,17 @@
-#include <iostream>
-#include <vector>
 using namespace std;
+#include <iostream>
 
+void InsertionSort( int arr[], int n){
+    for( int i =1; i < n; ++i){
+        int key = arr[i];
+        int j = i -1;
+        while( j >= 0 && arr[j] > key){
+            arr[j+1] = arr[j];
+            j=j-1;
 
-vector<int> insertion_sort(vector<int> nums){
-
-    for(int i =1; i< nums.size(); ++i){
-        int key = nums[i];
-
-        int j = i-1;
-
-        while(j >= 0&& nums[j] > key){
-
-            nums[j+1] = nums[j];
-
-            j = j - 1;
         }
 
-
-        nums[j + 1 ]= key; 
-
-        
-
+        arr[j+1] = key;
     }
-
-    return nums;
-
-
 }
 
-
-
-
-int main(){
-
-    vector<int> nums = {4,5,3,7,2,1,0};
-
-    vector<int> ans = insertion_sort(nums);
-
-    for(int i = 0; i < ans.size(); ++i){
-        cout <<"element["<<i<<"] is "<< ans[i]<< "\n";
-    } 
-
-    return 0;
-}
--- Rusty/rust_book/src/structs.rs
+++ Rusty/rust_book/src/structs.rs
@@ -1,36 +1,28 @@
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
-fn build_user(email: String, username: String)-> User{
-
-    User{
-        active:true,
-        username,
-        email,
-        sign_in_count: 1,
+impl Rectangle {
+    fn area(&self)-> u32{
+        self.width * self.height
     }
-
 }
+
 
 fn main(){
-    let mut user1 = User{
-        active: true,
-        username: String::from("johndoe123"),
-        email: String::from("someones@emai.com"),
-        sign_in_count: 1,
+
+    let rect1 = Rectangle{
+        width: 30,
+        height: 50,
     };
 
-    user1.email = String::from("someother dude");
+    println!("{rect1:#?}");
 
-    let user2 = User{
-        email: String::from("someother@email.com"),
-        ..user1
-    };
+    let res=rect1.area();
 
-    
+    println!("{res}");
 
 }
+
