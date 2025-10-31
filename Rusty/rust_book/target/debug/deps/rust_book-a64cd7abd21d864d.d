2fdf10a04be538ebac6db0e6e7dc38969be52ec8
SamuelCooper1105 <coopercumens@gmail.com>
2025-10-31 14:41:36 -0400

    removing target files

--- Rusty/rust_book/target/debug/deps/rust_book-a64cd7abd21d864d.d
+++
@@ -1,5 +0,0 @@
-/home/cooper/Projects/Rusty/rust_book/target/debug/deps/rust_book-a64cd7abd21d864d.d: src/main.rs
-
-/home/cooper/Projects/Rusty/rust_book/target/debug/deps/rust_book-a64cd7abd21d864d: src/main.rs
-
-src/main.rs:
--- Rusty/rust_book/target/debug/rust_book.d
+++
@@ -1,1 +0,0 @@
-/home/cooper/Projects/Rusty/rust_book/target/debug/rust_book: /home/cooper/Projects/Rusty/rust_book/src/main.rs
