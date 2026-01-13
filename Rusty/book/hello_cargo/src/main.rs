a9a799ba221810737b894f17d2902465fabd5a78
SAmuelCooper1105 <coopercumens@gmail.com>
2026-01-13 14:24:23 -0500

    checking stuff out with nvim

--- Rusty/book/hello_cargo/src/main.rs
+++ Rusty/book/hello_cargo/src/main.rs
@@ -1,3 +1,31 @@
 fn main() {
-    println!("Hello, world!");
+
+    let n = 5;
+
+    let ans = fib(n);
+
+    println!("{}",ans)
+    
+}
+
+fn fib(n: i32)->i32{
+    let mut a =0;
+    let mut b=1;
+    let mut c =0;
+
+   // 0,1,1,2,3,5,8,13
+
+    let counter = n-1;
+
+    if {
+    
+        let c = a + b;
+        let a = b;
+        let b=c;
+
+    }
+
+
+    return c
+
 }
