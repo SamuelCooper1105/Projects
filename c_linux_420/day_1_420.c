5ebf4f7ca0a58a0de1e2da5e0f9c296332ff6abc
SAmuelCooper1105 <coopercumens@gmail.com>
2026-01-13 15:13:49 -0500

    inclassexamplefor420

---
+++ c_linux_420/day_1_420.c
@@ -0,0 +1,19 @@
+#include <stdio,h>
+
+int main(int argc, char *argc[]){
+
+	int a[20]={}1,2,3};
+
+	int *ptr = a;
+
+	printf("%p %d\n", a *a);
+
+	ptr++;
+
+	printf("%p %d\n", ptr, *ptr);
+	printf("%p %d\n", (a+2), *(a+2));
+	printf("%p %d\n", (a+3),a[3]);
+
+	return 0;
+
+}
